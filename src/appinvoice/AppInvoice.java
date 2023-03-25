f9644e2ac2dcb464d57e400c39ba4b616657002b
Camilo Rodriguez <crodrigr@gmail.com>
2023-03-25 15:45:41 -0500

    Primero Proyecto

--- src/appinvoice/AppInvoice.java
+++ src/appinvoice/AppInvoice.java
@@ -13,6 +13,13 @@ public class AppInvoice {
                           +" \nDireccion: "+client.getAddress()
                           +" \nCelular: "+client.getNumberCell());
         
+        Client client1=new Client(1,"Juan","Perez","cra 34 No 56-11","353939333");
+        
+        System.out.println("Nombre: "+client1.getName()
+                          +" \nApellidos: "+client1.getLastName()
+                          +" \nDireccion: "+client1.getAddress()
+                          +" \nCelular: "+client1.getNumberCell());
+        
         
     }
     
